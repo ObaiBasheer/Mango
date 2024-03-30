e06750b2f573fdd2757d75dd15d29444f9f5bf5b
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2024-03-30 18:58:50 +0300

    Add Expertion Date for Coupon

--- Mango.Services.CouponAPI/Migrations/20240312094730_addExpertionsColmun.Designer.cs
+++ Mango.Services.CouponAPI/Migrations/20240312094730_addExpertionsColmun.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Mango.Services.CouponAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Mango.Services.CouponAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240312094730_addExpertionsColmun")]
+    partial class addExpertionsColmun
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Mango.Services.CouponAPI/Migrations/20240312094730_addExpertionsColmun.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addExpertionsColmun : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExeprationDate",
+                table: "coupons",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "coupons",
+                keyColumn: "CouponId",
+                keyValue: 1,
+                columns: new[] { "ExeprationDate", "LastUpdate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 3, 12, 9, 47, 29, 963, DateTimeKind.Utc).AddTicks(762) });
+
+            migrationBuilder.UpdateData(
+                table: "coupons",
+                keyColumn: "CouponId",
+                keyValue: 2,
+                columns: new[] { "ExeprationDate", "LastUpdate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 3, 12, 9, 47, 29, 963, DateTimeKind.Utc).AddTicks(809) });
+
+            migrationBuilder.UpdateData(
+                table: "coupons",
+                keyColumn: "CouponId",
+                keyValue: 3,
+                columns: new[] { "ExeprationDate", "LastUpdate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 3, 12, 9, 47, 29, 963, DateTimeKind.Utc).AddTicks(822) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExeprationDate",
+                table: "coupons");
+
+            migrationBuilder.UpdateData(
+                table: "coupons",
+                keyColumn: "CouponId",
+                keyValue: 1,
+                column: "LastUpdate",
+                value: new DateTime(2024, 2, 16, 10, 45, 58, 505, DateTimeKind.Utc).AddTicks(3042));
+
+            migrationBuilder.UpdateData(
+                table: "coupons",
+                keyColumn: "CouponId",
+                keyValue: 2,
+                column: "LastUpdate",
+                value: new DateTime(2024, 2, 16, 10, 45, 58, 505, DateTimeKind.Utc).AddTicks(3072));
+
+            migrationBuilder.UpdateData(
+                table: "coupons",
+                keyColumn: "CouponId",
+                keyValue: 3,
+                column: "LastUpdate",
+                value: new DateTime(2024, 2, 16, 10, 45, 58, 505, DateTimeKind.Utc).AddTicks(3082));
+        }
+    }
+}
