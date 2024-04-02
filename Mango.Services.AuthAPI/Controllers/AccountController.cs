27a7471cde403dd47f5637894dc4047ec5361e37
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2024-04-02 16:40:13 +0300

    add Account Controller with login and register methods

---
+++ Mango.Services.AuthAPI/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.AuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+            return Ok(ModelState);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok();
+        }
+    }
+}
