d6d920cf6d2c8f051f9c03f59549db3740d4c647
obai basheer <Oby.B@hotmail.com>
2024-04-08 14:20:02 +0300

    Add Login , register , UserDtos

---
+++ Mango.Services.AuthAPI/Models/Dtos/LoginResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Services.AuthAPI.Models.Dtos
+{
+    public record LoginResponseDto
+    {
+        public UserDto? User { get; set; }
+        public string? Token { get; set; }
+    }
+}
