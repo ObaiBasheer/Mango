fcb2f8575bf34c23c52db8ff75600b787cf1aaab
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2024-05-18 15:07:13 +0300

    Prepare Models and Dto

---
+++ Mango.Services.ShoppingCart/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        
+    }
+}
---
+++ Mango.Services.ShoppingCart/Models/Dtos/ResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.Dtos
+{
+    public record ResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }
+        public object? Result { get; set; }
+
+    }
+}
