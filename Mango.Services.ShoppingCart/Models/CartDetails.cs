60c5d7d8aafc5a01be413c8eaa9d94652bfbf408
Obai Basher <53649412+ObaiBasheer@users.noreply.github.com>
2024-05-27 23:37:59 +0300

    Add Propertiest to CartDetails in Shopping Cart Project

--- Mango.Services.ShoppingCart/Models/CartDetails.cs
+++ Mango.Services.ShoppingCart/Models/CartDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using Mango.Services.ShoppingCartAPI.Models.Dtos;
 
 namespace Mango.Services.ShoppingCartAPI.Models
 {
@@ -9,10 +10,10 @@ namespace Mango.Services.ShoppingCartAPI.Models
         public int CartDetailsId { get; set; }
         public int CartHeaderId { get; set; }
         [ForeignKey("CartHeaderId")]
-        public CartHeader CartHeader { get; set; }
+        public CartHeader? CartHeader { get; set; }
         public int ProductId { get; set; }
         [NotMapped]
-        public ProductDto Product { get; set; }
+        public ProductDto? Product { get; set; }
         public int Count { get; set; }
     }
 }
